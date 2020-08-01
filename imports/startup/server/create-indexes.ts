5e5396d1a8a1310181dfbafc402252840133bec4
Kwun Yeung <kwun@forbole.com>
2020-08-02 04:29:18 +0800

    Track block signers if they have signed the parent block (#34)
    * update packages
    
    * add celocli libraries
    
    * save signer records
    
    * add signer record db index
    
    * update CHANGELOG

--- imports/startup/server/create-indexes.ts
+++ imports/startup/server/create-indexes.ts
@@ -3,7 +3,7 @@ import { Blocks } from "../../api/blocks/blocks";
 import { Accounts } from "../../api/accounts/accounts";
 import { Transactions } from "../../api/transactions/transactions";
 import { ValidatorGroups } from "../../api/validator-groups/validator-groups";
-import { Validators } from "../../api/validators/validators";
+import { Validators, ValidatorRecords } from "../../api/validators/validators";
 import { Contracts } from "../../api/contracts/contracts";
 
 Blocks.rawCollection().createIndex({ number: -1 }, { unique: true });
@@ -14,4 +14,5 @@ Transactions.rawCollection().createIndex({ to: 1 });
 Transactions.rawCollection().createIndex({ from: 1 });
 ValidatorGroups.rawCollection().createIndex({ address: 1 }, { unique: true });
 Validators.rawCollection().createIndex({ address: 1 }, { unique: true });
+ValidatorRecords.rawCollection().createIndex({blockNumber:1, signer: 1}, {unique: true});
 Contracts.rawCollection().createIndex({ address: 1 }, { unique: true });
