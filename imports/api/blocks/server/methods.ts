273fac6e11af01b71aa99b385aac3fc19507909c
Kwun Yeung <kwun@forbole.com>
2020-08-01 20:10:52 +0000

    save signer records

--- imports/api/blocks/server/methods.ts
+++ imports/api/blocks/server/methods.ts
@@ -1,46 +1,48 @@
-import { Meteor } from "meteor/meteor";
-import { newKit } from "@celo/contractkit";
-import { Blocks } from "../blocks";
-import { Chain } from "../../chain/chain";
-import { Transactions } from "../../transactions/transactions";
+import { Meteor } from "meteor/meteor"
+import { newKit } from "@celo/contractkit"
+import { Blocks } from "../blocks"
+import { Chain } from "../../chain/chain"
+import { Transactions } from "../../transactions/transactions"
+import { ElectionResultsCache } from "@celo/celocli/lib/utils/election"
+import { ValidatorRecords } from "../../validators/validators"
 
-import PUB from "../../graphql/subscriptions";
+import PUB from "../../graphql/subscriptions"
 
-let kit = newKit(Meteor.settings.public.fornoAddress);
+let kit = newKit(Meteor.settings.public.fornoAddress)
 let web3 = kit.web3;
 
 Meteor.methods({
   "blocks.getBlocks": async function (targetHeight) {
-    console.log(targetHeight);
+    console.log(targetHeight)
     // this.unblock();
-    let latestBlockHeight = 0;
-    let latestBlock = Blocks.findOne({}, { sort: { number: -1 }, limit: 1 });
+    let latestBlockHeight = 0
+    let latestBlock = Blocks.findOne({}, { sort: { number: -1 }, limit: 1 })
     if (latestBlock) {
-      latestBlockHeight = latestBlock.number;
+      latestBlockHeight = latestBlock.number
     }
 
-    console.log("Last block in db: " + latestBlockHeight);
+    console.log("Last block in db: " + latestBlockHeight)
 
-    let chainId = await web3.eth.net.getId();
-    let lastBlock = latestBlock;
+    let chainId = await web3.eth.net.getId()
+    let lastBlock = latestBlock
 
     for (let i = latestBlockHeight + 1; i <= targetHeight; i++) {
-      let blockTime = 0;
+      let blockTime = 0
       try {
-        console.log("Processing block: " + i);
+        console.log("Processing block: " + i)
         // get block
-        let block: { [k: string]: any } = await web3.eth.getBlock(i);
+        let block: { [k: string]: any } = await web3.eth.getBlock(i)
 
-        if (!block) return i;
+        if (!block) return i
 
         // console.log(block);
         if (lastBlock) {
           // console.log(block);
-          blockTime = block.timestamp - lastBlock.timestamp;
+          blockTime = block.timestamp - lastBlock.timestamp
         }
 
         // calculate block time
-        block.blockTime = blockTime;
+        block.blockTime = blockTime
 
         let chainState: { [k: string]: any } = Chain.findOne({
           chainId: chainId,
@@ -50,23 +52,44 @@ Meteor.methods({
 
           // make sure averageBlockTime and txCount exist before calculation
 
-          if (!chainState.averageBlockTime) chainState.averageBlockTime = 0;
-          if (!chainState.txCount) chainState.txCount = 0;
+          if (!chainState.averageBlockTime) chainState.averageBlockTime = 0
+          if (!chainState.txCount) chainState.txCount = 0
           
-          chainState.averageBlockTime = (chainState.averageBlockTime * (i - 1) + blockTime) / i;
-          chainState.latestHeight = block.number;
+          chainState.averageBlockTime = (chainState.averageBlockTime * (i - 1) + blockTime) / i
+          chainState.latestHeight = block.number
         } else {
-          chainState = {};
-          chainState.averageBlockTime = 0;
-          chainState.txCount = 0;
+          chainState = {}
+          chainState.averageBlockTime = 0
+          chainState.txCount = 0
         }
 
-        chainState.latestHeight = block.number;
+        chainState.latestHeight = block.number
+
+        // get block singer records
+        
+        const epochNumber = await kit.getEpochNumberOfBlock(block.number)
+        const election = await kit.contracts.getElection()
+        const validatorSet = await election.getElectedValidators(epochNumber)
+        const validators = await kit.contracts.getValidators()
+        const epochSize = await validators.getEpochSize()
+        // console.log(validatorSet)
+
+        const electionRC = new ElectionResultsCache(election, epochSize.toNumber())
+        for (let v in validatorSet){
+          let record:any = {
+            blockNumber: block.number,
+            signer: validatorSet[v].signer,
+            exist: await electionRC.signedParent(validatorSet[v].signer, block)
+          }
+          ValidatorRecords.insert(record);
+        }
+        // const blockExtraData = parseBlockExtraData(block.extraData);
+        // console.log(blockExtraData);
 
         // get transactions hash
         if (block.transactions.length > 0) {
           for (let j = 0; j < block.transactions.length; j++) {
-            console.log("Add pending transaction: " + block.transactions[j]);
+            console.log("Add pending transaction: " + block.transactions[j])
             let tx = {
               hash: block.transactions[j],
               pending: true,
@@ -74,7 +97,7 @@ Meteor.methods({
               blockHash: block.hash,
             };
             if (tx) {
-              console.log("Processing transaction: " + tx.hash);
+              console.log("Processing transaction: " + tx.hash)
               // insert tx
               try {
                 Transactions.insert(tx, (error, result) => {
@@ -83,25 +106,25 @@ Meteor.methods({
                   });
                 });
               } catch (e) {
-                console.log(e);
+                console.log(e)
               }
             }
           }
-          chainState.txCount += block.transactions.length;
+          chainState.txCount += block.transactions.length
         }
 
         // console.log(chainState)
-        Chain.upsert({ chainId: chainId }, { $set: chainState });
+        Chain.upsert({ chainId: chainId }, { $set: chainState })
         Blocks.insert(block, (error, result) => {
-          PUB.pubsub.publish(PUB.BLOCK_ADDED, { blockAdded: block });
+          PUB.pubsub.publish(PUB.BLOCK_ADDED, { blockAdded: block })
         });
 
-        lastBlock = block;
+        lastBlock = block
       } catch (e) {
-        console.log(e);
+        console.log(e)
       }
     }
 
-    return targetHeight;
+    return targetHeight
   },
 });
--- imports/api/validators/validators.ts
+++ imports/api/validators/validators.ts
@@ -1,2 +1,3 @@
 import { Mongo } from 'meteor/mongo';
 export const Validators = new Mongo.Collection('validators');
+export const ValidatorRecords = new Mongo.Collection('validators_records');
