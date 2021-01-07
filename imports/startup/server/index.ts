650311231a1bd9e3b2fab7649a502619000a9d48
MonikaCat <pusz.monika@gmail.com>
2021-01-07 13:11:54 +0000

    WIP: ddp-apollo

--- imports/startup/server/index.ts
+++ imports/startup/server/index.ts
@@ -1,28 +1,15 @@
-
-// Import server side methods
-import './register-apis';
-// Create collection indexes
-import './create-indexes';
-
-import { ApolloServer } from 'apollo-server-express'
-import { WebApp } from 'meteor/webapp'
-
 import typeDefs from '../../api/graphql/schema';
 import resolvers from '../../api/graphql/resolvers';
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+import { WebApp } from 'meteor/webapp';
+import { execute, subscribe } from 'graphql';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
 
-server.applyMiddleware({
-    app: WebApp.connectHandlers,
-    path: '/graphql'
-})
-
-server.installSubscriptionHandlers(WebApp.httpServer);
-  
-WebApp.connectHandlers.use('/graphql', (req, res) => {
-    if (req.method === 'GET') {
-      res.end()
-    }
-})
+new SubscriptionServer({
+  schema: typeDefs,
+  execute,
+  subscribe,
+}, {
+  server: WebApp.httpServer,
+  path: '/subscriptions',
+});
