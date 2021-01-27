622c222785ded6a229e68d0c77c6b69cc8565ada
MonikaCat <pusz.monika@gmail.com>
2021-01-27 05:26:08 +0000

    Updated methods.ts in contracts/server

--- imports/api/contracts/server/methods.ts
+++ imports/api/contracts/server/methods.ts
@@ -14,22 +14,15 @@ import { ABI as FeeCurrencyWhitelistABI } from "@celo/contractkit/lib/generated/
 import { ABI as FreezerABI } from "@celo/contractkit/lib/generated/Freezer";
 import { ABI as GasPriceMinimumABI } from "@celo/contractkit/lib/generated/GasPriceMinimum";
 import { ABI as GoldTokenABI } from "@celo/contractkit/lib/generated/GoldToken";
-import { ABI as GovernanceApproverMultiSigABI } from "@celo/contractkit/lib/generated/GovernanceApproverMultiSig";
 import { ABI as GovernanceABI } from "@celo/contractkit/lib/generated/Governance";
 import { ABI as LockedGoldABI } from "@celo/contractkit/lib/generated/LockedGold";
-import { ABI as MultiSigABI } from "@celo/contractkit/lib/generated/MultiSig";
-import { ABI as ProxyABI } from "@celo/contractkit/lib/generated/Proxy";
 import { ABI as RandomABI } from "@celo/contractkit/lib/generated/Random";
 import { ABI as RegistryABI } from "@celo/contractkit/lib/generated/Registry";
-import { ABI as ReleaseGoldABI } from "@celo/contractkit/lib/generated/ReleaseGold";
 import { ABI as ReserveABI } from "@celo/contractkit/lib/generated/Reserve";
-import { ABI as ReserveSpenderMultiSigABI } from "@celo/contractkit/lib/generated/ReserveSpenderMultiSig";
 import { ABI as SortedOraclesABI } from "@celo/contractkit/lib/generated/SortedOracles";
 import { ABI as StableTokenABI } from "@celo/contractkit/lib/generated/StableToken";
 import { ABI as TransferWhitelistABI } from "@celo/contractkit/lib/generated/TransferWhitelist";
 import { ABI as ValidatorsABI } from "@celo/contractkit/lib/generated/Validators";
-
-
 import { Contracts } from "../../contracts/contracts";
 
 let kit = newKit(Meteor.settings.public.fornoAddress);
@@ -38,36 +31,188 @@ let web3 = kit.web3;
 Meteor.methods({
   "contract.getContractAddress": async function () {
     let contracts = {};
+    let contractABI = {};
+
+    //Save contracts address
+
+    // Accounts
     try {
       contracts["Accounts"] = await kit.registry.addressFor(CeloContract.Accounts);
-      contracts["Attestations"] = await kit.registry.addressFor(CeloContract.Attestations);
-      contracts["BlockchainParameters"] = await kit.registry.addressFor(CeloContract.BlockchainParameters);
-      contracts["DoubleSigningSlasher"] = await kit.registry.addressFor(CeloContract.DoubleSigningSlasher);
-      contracts["DowntimeSlasher"] = await kit.registry.addressFor(CeloContract.DowntimeSlasher);
-      contracts["Election"] = await kit.registry.addressFor(CeloContract.Election);
-      contracts["EpochRewards"] = await kit.registry.addressFor(CeloContract.EpochRewards);
-      contracts["Escrow"] = await kit.registry.addressFor(CeloContract.Escrow);
-      contracts["Exchange"] = await kit.registry.addressFor(CeloContract.Exchange);
-      contracts["FeeCurrencyWhitelist"] = await kit.registry.addressFor(CeloContract.FeeCurrencyWhitelist);
-      contracts["Freezer"] = await kit.registry.addressFor(CeloContract.Freezer);
-      contracts["GasPriceMinimum"] = await kit.registry.addressFor(CeloContract.GasPriceMinimum);
-      contracts["GoldToken"] = await kit.registry.addressFor(CeloContract.GoldToken);
-      contracts["Governance"] = await kit.registry.addressFor(CeloContract.Governance);
-      contracts["LockedGold"] = await kit.registry.addressFor(CeloContract.LockedGold);
-      contracts["Random"] = await kit.registry.addressFor(CeloContract.Random);
-      contracts["Registry"] = await kit.registry.addressFor(CeloContract.Registry);
-      contracts["Reserve"] = await kit.registry.addressFor(CeloContract.Reserve);
-      contracts["SortedOracles"] = await kit.registry.addressFor(CeloContract.SortedOracles);
-      contracts["StableToken"] = await kit.registry.addressFor(CeloContract.StableToken);
-      contracts["TransferWhitelist"] = await kit.registry.addressFor(CeloContract.TransferWhitelist);
-      contracts["Validators"] = await kit.registry.addressFor(CeloContract.Validators);
-
     }
     catch (error) {
-      console.log("Error when getting Contract Address " + error)
+      console.log("Error when getting Accounts Contract Address " + error)
     }
 
-    let contractABI = {};
+    // Attestations
+    try {
+      contracts["Attestations"] = await kit.registry.addressFor(CeloContract.Attestations);
+    }
+    catch (error) {
+      console.log("Error when getting Attestations Contract Address " + error)
+    }
+
+    // BlockchainParameters
+    try {
+      contracts["BlockchainParameters"] = await kit.registry.addressFor(CeloContract.BlockchainParameters);
+    }
+    catch (error) {
+      console.log("Error when getting Blockchain Parameters Contract Address " + error)
+    }
+
+    // DoubleSigningSlasher
+    try {
+      contracts["DoubleSigningSlasher"] = await kit.registry.addressFor(CeloContract.DoubleSigningSlasher);
+    }
+    catch (error) {
+      console.log("Error when getting Double Signing Slasher Contract Address " + error)
+    }
+    
+    // DowntimeSlasher
+    try {
+      contracts["DowntimeSlasher"] = await kit.registry.addressFor(CeloContract.DowntimeSlasher);
+    }
+    catch (error) {
+      console.log("Error when getting Downtime Slasher Contract Address " + error)
+    }
+
+    // Election
+    try {
+      contracts["Election"] = await kit.registry.addressFor(CeloContract.Election);
+    }
+    catch (error) {
+      console.log("Error when getting Election Contract Address " + error)
+    }
+
+    // EpochRewards
+    try {
+      contracts["EpochRewards"] = await kit.registry.addressFor(CeloContract.EpochRewards);
+    }
+    catch (error) {
+      console.log("Error when getting EpochRewards Contract Address " + error)
+    }
+
+    // Escrow
+    try {
+      contracts["Escrow"] = await kit.registry.addressFor(CeloContract.Escrow);
+    }
+    catch (error) {
+      console.log("Error when getting Escrow Contract Address " + error)
+    }
+
+    // Exchange
+    try {
+      contracts["Exchange"] = await kit.registry.addressFor(CeloContract.Exchange);
+    }
+    catch (error) {
+      console.log("Error when getting Exchange Contract Address " + error)
+    }
+
+    // FeeCurrencyWhitelist
+    try {
+      contracts["FeeCurrencyWhitelist"] = await kit.registry.addressFor(CeloContract.FeeCurrencyWhitelist);
+    }
+    catch (error) {
+      console.log("Error when getting Fee Currency Whitelist Contract Address " + error)
+    }
+
+    // Freezer
+    try {
+      contracts["Freezer"] = await kit.registry.addressFor(CeloContract.Freezer);
+    }
+    catch (error) {
+      console.log("Error when getting Freezer Contract Address " + error)
+    }
+
+    // GasPriceMinimum
+    try {
+      contracts["GasPriceMinimum"] = await kit.registry.addressFor(CeloContract.GasPriceMinimum);
+    }
+    catch (error) {
+      console.log("Error when getting Gas Price Minimum Contract Address " + error)
+    }
+
+    // GoldToken
+    try {
+      contracts["GoldToken"] = await kit.registry.addressFor(CeloContract.GoldToken);
+    }
+    catch (error) {
+      console.log("Error when getting Gold Token Contract Address " + error)
+    }
+
+    // Governance
+    try {
+      contracts["Governance"] = await kit.registry.addressFor(CeloContract.Governance);
+    }
+    catch (error) {
+      console.log("Error when getting Governance Contract Address " + error)
+    }
+
+    // LockedGold
+    try {
+      contracts["LockedGold"] = await kit.registry.addressFor(CeloContract.LockedGold);
+    }
+    catch (error) {
+      console.log("Error when getting Locked Gold Contract Address " + error)
+    }
+
+    // Random
+    try {
+      contracts["Random"] = await kit.registry.addressFor(CeloContract.Random);
+    }
+    catch (error) {
+      console.log("Error when getting Random Contract Address " + error)
+    }
+
+    // Registry
+    try {
+      contracts["Registry"] = await kit.registry.addressFor(CeloContract.Registry);
+    }
+    catch (error) {
+      console.log("Error when getting Registry Contract Address " + error)
+    }
+
+    // Reserve
+    try {
+      contracts["Reserve"] = await kit.registry.addressFor(CeloContract.Reserve);
+    }
+    catch (error) {
+      console.log("Error when getting Reserve Contract Address " + error)
+    }
+
+    // SortedOracles
+    try {
+      contracts["SortedOracles"] = await kit.registry.addressFor(CeloContract.SortedOracles);
+    }
+    catch (error) {
+      console.log("Error when getting Sorted Oracles Contract Address " + error)
+    }
+
+    // StableToken
+    try {
+      contracts["StableToken"] = await kit.registry.addressFor(CeloContract.StableToken);
+    }
+    catch (error) {
+      console.log("Error when getting Stable Token Contract Address " + error)
+    }
+
+    // TransferWhitelist
+    try {
+      contracts["TransferWhitelist"] = await kit.registry.addressFor(CeloContract.TransferWhitelist);
+    }
+    catch (error) {
+      console.log("Error when getting Transfer Whitelist Contract Address " + error)
+    }
+
+    // Validators
+    try {
+      contracts["Validators"] = await kit.registry.addressFor(CeloContract.Validators);
+    }
+    catch (error) {
+      console.log("Error when getting Validators Contract Address " + error)
+    }
+
+
+    // Save contracts ABI 
     contractABI["Accounts"] = AccountsABI
     contractABI["Attestations"] = AttestationsABI
     contractABI["BlockchainParameters"] = BlockchainParametersABI
