08fc973dea4c0102d0525851932fe2fdff63937e
MonikaCat <pusz.monika@gmail.com>
2021-02-17 06:05:31 +0000

    Updated index.ts

--- imports/startup/server/index.ts
+++ imports/startup/server/index.ts
@@ -4,25 +4,16 @@ import './register-apis';
 // Create collection indexes
 import './create-indexes';
 
-import { ApolloServer } from 'apollo-server-express'
 import { WebApp } from 'meteor/webapp'
 
 import typeDefs from '../../api/graphql/schema';
 import resolvers from '../../api/graphql/resolvers';
+import { ApolloServer, gql } from "apollo-server"
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
 
-server.applyMiddleware({
-    app: WebApp.connectHandlers,
-    path: '/graphql'
-})
-
-// server.installSubscriptionHandlers(WebApp.httpServer);
-  
-WebApp.connectHandlers.use('/graphql', (req, res) => {
-    if (req.method === 'GET') {
-      res.end()
-    }
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
 })
