165cb343b599b9ab667b88632da7387ce2307956
Magic Cat <pusz.monika@gmail.com>
2021-02-25 00:38:07 +0800

    Updated txs

--- imports/api/transactions/server/methods.ts
+++ imports/api/transactions/server/methods.ts
@@ -1,13 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import { Transactions } from '../../transactions/transactions';
-import { Contracts } from '../../contracts/contracts';
 import { newKit } from '@celo/contractkit';
 import * as abiDecoder from 'abi-decoder';
+import Transactions from '../transactions';
+import Contracts from '../../contracts/contracts';
 
 import PUB from '../../graphql/subscriptions';
 
-let kit = newKit(Meteor.settings.public.fornoAddress);
-let web3 = kit.web3;
+const kit = newKit(Meteor.settings.public.fornoAddress);
+const { web3 } = kit;
 
 interface PendingTransactionsInterface {
     [i: string]: any;
@@ -20,111 +20,117 @@ interface ContractInterface {
 
 }
 
-
 const updatePendingTransactions = (tx) => {
-    Meteor.call('accounts.update', tx?.from, (error, result) => {
-        if (error) {
-            console.log("Error when processing Pending Transactions " + error)
-        }
-        if (result) {
-            console.log(result)
-        }
-    });
+  Meteor.call('accounts.update', tx?.from, (error, result) => {
+    if (error) {
+      console.log(`Error when processing Pending Transactions ${error}`);
+    }
+    if (result) {
+      console.log(result);
+    }
+  });
 };
 
 const updateTransactions = (tx) => {
-     Meteor.call('accounts.update', tx?.to, (error, result) => {
-        if (error) {
-            console.log("Error when updating Account Transactions (Transfer) " + error)
-        }
-        if (result) {
-            console.log(result)
-        }
-    })
+  Meteor.call('accounts.update', tx?.to, (error, result) => {
+    if (error) {
+      console.log(`Error when updating Account Transactions (Transfer) ${error}`);
+    }
+    if (result) {
+      console.log(result);
+    }
+  });
 };
 
 const decodeTransaction = (tx) => {
-        // make sure it has input in the txn
-        let contract: ContractInterface = Contracts.findOne({ address: tx.to });
-        if (!!contract && !!contract.ABI) {
-            abiDecoder.addABI(contract.ABI);
-            let decodedInput = abiDecoder.decodeMethod(tx.input);
+  // make sure it has input in the txn
+  const contract: ContractInterface = Contracts.findOne({
+    address: tx.to,
+  });
+  if (!!contract && !!contract.ABI) {
+    abiDecoder.addABI(contract.ABI);
+    const decodedInput = abiDecoder.decodeMethod(tx.input);
 
-            if (decodedInput && decodedInput.name) {
-                for (let i in decodedInput.params) {
-                    if (decodedInput.params[i].type == 'address') {
-                        Meteor.call('accounts.update', decodedInput.params[i].value, (error, result) => {
-                            if (error) {
-                                console.log("Error when updating Account Decoded Tx " + error)
-                            }
-                            if (result) {
-                                console.log(result)
-                            }
-                        })
-                    }
-                }
+    if (decodedInput && decodedInput.name) {
+      for (const i in decodedInput.params) {
+        if (decodedInput.params[i].type === 'address') {
+          Meteor.call('accounts.update', decodedInput.params[i].value, (error, result) => {
+            if (error) {
+              console.log(`Error when updating Account Decoded Tx ${error}`);
             }
+            if (result) {
+              console.log(result);
+            }
+          });
+        }
+      }
+    }
 
-            if (decodedInput) {
-                tx.decodedInput = decodedInput;
-                tx.type = decodedInput.name;
-            }
-        }
-        else {
-            tx.type = 'contractCall'
-        }
+    if (decodedInput) {
+      tx.decodedInput = decodedInput;
+      tx.type = decodedInput.name;
+    }
+  } else {
+    tx.type = 'contractCall';
+  }
 };
 
 Meteor.methods({
-    'transactions.updatePending': async function () {
-        this.unblock()
-        let pendingTransactions: PendingTransactionsInterface = Transactions.find({ pending: true }).fetch();
-        for (let i in pendingTransactions) {
-            let tx: { [k: string]: any };
-            try {
-                tx = pendingTransactions[i] && pendingTransactions[i].hash ? await web3.eth.getTransaction(pendingTransactions[i].hash) : null;
-            }
-            catch (error) {
-                console.log("Error when updating Pending Transactions " + error)
-            }
-
-            if (tx) {
-                console.log("Processing pending transaction: " + tx.hash);
-                // insert tx
-                try {
-                   updatePendingTransactions(tx)
-                   
-                    if (parseInt(tx.value) > 0) {
-                        // this is a money transfer txn
-                        // store the recepient account
-                        if (tx.to) {
-                           updateTransactions(tx)
-                        }
-                        tx.type = 'transfer';
-                    }
-                    else {
-                        // this is a contract call
-                        // the tx address will be the contract address
-
-                        if (tx.input != "0x") {
-                            decodeTransaction(tx)
-                        }
-
-                        if (tx.to) {
-                           updateTransactions(tx)
-                        }
-                    }
-
-                    tx.pending = false;
-                    Transactions.update({ hash: tx.hash }, { $set: tx }, (error: any, result: any) => {
-                        PUB.pubsub.publish(PUB.TRANSACTION_ADDED, { transactionAdded: tx });
-                    });
-
-                }
-                catch (e) {
-                    console.log("Error when trying to process Pending Transactions " + e)
-                }
-            }
+  'transactions.updatePending': async function () {
+    this.unblock();
+    const pendingTransactions: PendingTransactionsInterface = Transactions.find({
+      pending: true,
+    }).fetch();
+    for (const i in pendingTransactions) {
+      if (i) {
+        let tx: { [k: string]: any };
+        try {
+          tx = pendingTransactions[i] && pendingTransactions[i].hash ? await web3.eth.getTransaction(pendingTransactions[i].hash) : null;
+        } catch (error) {
+          console.log(`Error when updating Pending Transactions ${error}`);
         }
+
+        if (tx) {
+          console.log(`Processing pending transaction: ${tx.hash}`);
+          // insert tx
+          try {
+            updatePendingTransactions(tx);
+
+            if (parseInt(tx.value) > 0) {
+            // this is a money transfer txn
+            // store the recepient account
+              if (tx.to) {
+                updateTransactions(tx);
+              }
+              tx.type = 'transfer';
+            } else {
+            // this is a contract call
+            // the tx address will be the contract address
+
+              if (tx.input !== '0x') {
+                decodeTransaction(tx);
+              }
+
+              if (tx.to) {
+                updateTransactions(tx);
+              }
+            }
+
+            tx.pending = false;
+            Transactions.update({
+              hash: tx.hash,
+            }, {
+              $set: tx,
+            }, (error: any, result: any) => {
+              PUB.pubsub.publish(PUB.TRANSACTION_ADDED, {
+                transactionAdded: tx,
+              });
+            });
+          } catch (e) {
+            console.log(`Error when trying to process Pending Transactions ${e}`);
+          }
+        }
+      }
     }
+  },
 });
