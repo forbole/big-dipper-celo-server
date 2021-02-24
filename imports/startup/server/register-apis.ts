b9c235f287130daa438c993b6ed821da2cef33db
Magic Cat <pusz.monika@gmail.com>
2021-02-25 00:36:51 +0800

    Updated startup

--- imports/startup/server/register-apis.ts
+++ imports/startup/server/register-apis.ts
@@ -1,8 +1,8 @@
 // register server side methods
-import "../../api/blocks/server/methods";
-import "../../api/transactions/server/methods";
-import "../../api/chain/server/methods";
-import "../../api/validators/server/methods";
-import "../../api/accounts/server/methods";
-import "../../api/contracts/server/methods";
-import "../../api/governance/server/methods";
+import '../../api/blocks/server/methods';
+import '../../api/transactions/server/methods';
+import '../../api/chain/server/methods';
+import '../../api/validators/server/methods';
+import '../../api/accounts/server/methods';
+import '../../api/contracts/server/methods';
+import '../../api/governance/server/methods';
